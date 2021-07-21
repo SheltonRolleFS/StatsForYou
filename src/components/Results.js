315e0636e1b16db474074b458603acd31472981e
Shelton Rolle <sheltonrolle@Sheltons-MacBook-Pro-2.local>
2021-07-21 17:10:37 -0400

    The fetch requests are now in their own function and return an object containing the requested channels information. An onClick function was created to be called instead of the fetch function and when the button is clicked. The onClick function calls the fetchData function and needs to set the user state with the data collected from the fetch.

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,19 +1,21 @@
 import React from 'react'
 
-const Result = ({ data, errMsg }) => {
-    if(data === errMsg){
-        return (
-            <section className="results">
-                <p className="results__err-msg">{data}</p>
-            </section>
-        )
-    }else{
-        return (
-            <section className="results">
-                <h1>{data}</h1>
-            </section>
-        )
-    }
+const Result = ({ name, errMsg }) => {
+    // if(name === errMsg){
+    //     return (
+    //         <section className="results">
+    //             <p className="results__err-msg">{errMsg}</p>
+    //         </section>
+    //     )
+    // }else{
+        
+    // }
+
+    return (
+        <section className="results">
+            <h1>{name}</h1>
+        </section>
+    )
 }
 
 export default Result
