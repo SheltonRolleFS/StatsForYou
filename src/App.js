56924bd1ab773a87383dbae68628ef428d7b591f
Shelton Rolle <sheltonrolle@Sheltons-MacBook-Pro-2.local>
2021-07-21 04:46:50 -0400

    The html elements that will never change have been added. These are the user input section and the about section which describes the application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <section className="user-input">
+          <div className="user-input__entry">
+            <label htmlFor="channelName">Channel Name</label>
+            <input type="text" id="channelName" placeholder="Enter channel name here..."/>
+          </div>
+          <button>Search</button>
+        </section>
+
+        <section className="about">
+          <h2>What is Stats4You?</h2>
+          <p>
+              Stats4You is a website made to provide you information on your 
+              favorite Twitch streamers. Using the TwitchAPI, and the username of a 
+              streamer of your choosing, we gather relative information such as whether
+              the streamer is currently live, information on their stream if they are 
+              live, and a list of all of their emotes. 
+          </p>
+          <p>You can check out the Twitch API documentation <a href="https://dev.twitch.tv/docs/api/" target="_blank" rel="noreferrer">HERE</a></p>
+        </section>
+      </div>
+    </>
   );
 }
 
